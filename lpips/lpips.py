ce78a7f48682aa997724c5adb93fe71662c8a884
delldu <18588220928@163.com>
2021-08-05 00:29:31 +0800

    Add readme.

--- lpips/lpips.py
+++ lpips/lpips.py
@@ -1,229 +1,140 @@
-
-from __future__ import absolute_import
-
 import torch
 import torch.nn as nn
-import torch.nn.init as init
-from torch.autograd import Variable
 import numpy as np
-import pretrained_networks as pn
-import torch.nn
+import model_helper as pn
+
+import onnx
 import onnxruntime
+
 from PIL import Image
 import torchvision.transforms as transforms
 
-import lpips
 import pdb
 
-def spatial_average(in_tens, keepdim=True):
-    return in_tens.mean([2,3],keepdim=keepdim)
 
-def upsample(in_tens, out_HW=(64,64)): # assumes scale factor is same for H and W
+def upsample(in_tens, out_HW=(64, 64)):  # assumes scale factor is same for H and W
     in_H, in_W = in_tens.shape[2], in_tens.shape[3]
-    return nn.Upsample(size=out_HW, mode='bilinear', align_corners=False)(in_tens)
+    return nn.Upsample(size=out_HW, mode="bilinear", align_corners=False)(in_tens)
 
-def normalize_tensor(in_feat,eps=1e-10):
-    norm_factor = torch.sqrt(torch.sum(in_feat**2,dim=1,keepdim=True))
-    return in_feat/(norm_factor+eps)
+
+def normalize_tensor(in_feat, eps=1e-10):
+    norm_factor = torch.sqrt(torch.sum(in_feat ** 2, dim=1, keepdim=True))
+    return in_feat / (norm_factor + eps)
+
 
 # Learned perceptual metric
 class LPIPS(nn.Module):
-    def __init__(self, pretrained=True, net='alex', version='0.1', lpips=True, spatial=False, 
-        pnet_rand=False, pnet_tune=False, use_dropout=True, model_path=None, eval_mode=True, verbose=True):
-        # lpips - [True] means with linear calibration on top of base network
-        # pretrained - [True] means load linear weights
-
+    def __init__(self, net="alex"):
         super(LPIPS, self).__init__()
-        if(verbose):
-            print('Setting up [%s] perceptual loss: trunk [%s], v[%s], spatial [%s]'%
-                ('LPIPS' if lpips else 'baseline', net, version, 'on' if spatial else 'off'))
+
+        version = "0.1"
 
         self.pnet_type = net
-        self.pnet_tune = pnet_tune
-        self.pnet_rand = pnet_rand
-        self.spatial = spatial
-        self.lpips = lpips # false means baseline of just averaging all layers
         self.version = version
         self.scaling_layer = ScalingLayer()
 
-        if(self.pnet_type in ['vgg','vgg16']):
+        if self.pnet_type in ["vgg", "vgg16"]:
             net_type = pn.vgg16
-            self.chns = [64,128,256,512,512]
-        elif(self.pnet_type=='alex'):
+            self.channels = [64, 128, 256, 512, 512]
+        elif self.pnet_type == "alex":
             net_type = pn.alexnet
-            self.chns = [64,192,384,256,256]
-        elif(self.pnet_type=='squeeze'):
+            self.channels = [64, 192, 384, 256, 256]
+        elif self.pnet_type == "squeeze":
             net_type = pn.squeezenet
-            self.chns = [64,128,256,384,384,512,512]
-        self.L = len(self.chns)
+            self.channels = [64, 128, 256, 384, 384, 512, 512]
+        self.L = len(self.channels)
 
-        self.net = net_type(pretrained=not self.pnet_rand, requires_grad=self.pnet_tune)
+        self.net = net_type(pretrained=True, requires_grad=False)
 
-        if(lpips):
-            self.lin0 = NetLinLayer(self.chns[0], use_dropout=use_dropout)
-            self.lin1 = NetLinLayer(self.chns[1], use_dropout=use_dropout)
-            self.lin2 = NetLinLayer(self.chns[2], use_dropout=use_dropout)
-            self.lin3 = NetLinLayer(self.chns[3], use_dropout=use_dropout)
-            self.lin4 = NetLinLayer(self.chns[4], use_dropout=use_dropout)
-            self.lins = [self.lin0,self.lin1,self.lin2,self.lin3,self.lin4]
-            if(self.pnet_type=='squeeze'): # 7 layers for squeezenet
-                self.lin5 = NetLinLayer(self.chns[5], use_dropout=use_dropout)
-                self.lin6 = NetLinLayer(self.chns[6], use_dropout=use_dropout)
-                self.lins+=[self.lin5,self.lin6]
-            self.lins = nn.ModuleList(self.lins)
+        self.lin0 = NetLinLayer(self.channels[0])
+        self.lin1 = NetLinLayer(self.channels[1])
+        self.lin2 = NetLinLayer(self.channels[2])
+        self.lin3 = NetLinLayer(self.channels[3])
+        self.lin4 = NetLinLayer(self.channels[4])
+        self.lins = [self.lin0, self.lin1, self.lin2, self.lin3, self.lin4]
+        if self.pnet_type == "squeeze":  # 7 layers for squeezenet
+            self.lin5 = NetLinLayer(self.channels[5])
+            self.lin6 = NetLinLayer(self.channels[6])
+            self.lins += [self.lin5, self.lin6]
+        self.lins = nn.ModuleList(self.lins)
 
-            if(pretrained):
-                if(model_path is None):
-                    import inspect
-                    import os
-                    model_path = os.path.abspath(os.path.join(inspect.getfile(self.__init__), '..', 'weights/v%s/%s.pth'%(version,net)))
+        import inspect
+        import os
 
-                if(verbose):
-                    print('Loading model from: %s'%model_path)
-                self.load_state_dict(torch.load(model_path, map_location='cpu'), strict=False)          
+        model_path = os.path.abspath(
+            os.path.join(
+                inspect.getfile(self.__init__),
+                "..",
+                "weights/v%s/%s.pth" % (version, net),
+            )
+        )
 
-        if(eval_mode):
-            self.eval()
+        print("Loading model from: %s" % model_path)
+        self.load_state_dict(torch.load(model_path, map_location="cpu"), strict=False)
 
-    def forward(self, input, normalize=True):
-        in0 = input[0:1]
-        in1 = input[1:2]
-        if normalize: # turn on this flag if input is [0,1] so it can be adjusted to [-1, +1]
-            in0 = 2 * in0  - 1
-            in1 = 2 * in1  - 1
+    def forward(self, input):
+        image_0 = input[0:1]
+        image_1 = input[1:2]
 
-        # v0.0 - original release had a bug, where input was not scaled
-        in0_input, in1_input = (self.scaling_layer(in0), self.scaling_layer(in1)) if self.version=='0.1' else (in0, in1)
-        outs0, outs1 = self.net.forward(in0_input), self.net.forward(in1_input)
-        feats0, feats1, diffs = {}, {}, {}
+        # Move input from [0,1] [-1, +1]
+        image_0 = 2 * image_0 - 1
+        image_1 = 2 * image_1 - 1
 
-        for kk in range(self.L):
-            feats0[kk], feats1[kk] = normalize_tensor(outs0[kk]), normalize_tensor(outs1[kk])
-            diffs[kk] = (feats0[kk]-feats1[kk])**2
+        input_0 = self.scaling_layer(image_0)
+        input_1 = self.scaling_layer(image_1)
 
-        if(self.lpips):
-            if(self.spatial):
-                res = [upsample(self.lins[kk](diffs[kk]), out_HW=in0.shape[2:]) for kk in range(self.L)]
-            else:
-                res = [spatial_average(self.lins[kk](diffs[kk]), keepdim=True) for kk in range(self.L)]
-        else:
-            if(self.spatial):
-                res = [upsample(diffs[kk].sum(dim=1,keepdim=True), out_HW=in0.shape[2:]) for kk in range(self.L)]
-            else:
-                res = [spatial_average(diffs[kk].sum(dim=1,keepdim=True), keepdim=True) for kk in range(self.L)]
+        output_0 = self.net.forward(input_0)
+        output_1 = self.net.forward(input_1)
+        features_0, features_1, difference = {}, {}, {}
+
+        for k in range(self.L):
+            features_0[k] = normalize_tensor(output_0[k])
+            features_1[k] = normalize_tensor(output_1[k])
+
+            difference[k] = (features_0[k] - features_1[k]) ** 2
+
+        res = [
+            upsample(self.lins[k](difference[k]), out_HW=image_0.shape[2:])
+            for k in range(self.L)
+        ]
 
         val = res[0]
-        for l in range(1,self.L):
+        for l in range(1, self.L):
             val += res[l]
 
-        # a = spatial_average(self.lins[kk](diffs[kk]), keepdim=True)
-        # b = torch.max(self.lins[kk](feats0[kk]**2))
-        # for kk in range(self.L):
-        #     a += spatial_average(self.lins[kk](diffs[kk]), keepdim=True)
-        #     b = torch.max(b,torch.max(self.lins[kk](feats0[kk]**2)))
-        # a = a/self.L
-        # from IPython import embed
-        # embed()
-        # return 10*torch.log10(b/a)
-        
         return val
 
 
 class ScalingLayer(nn.Module):
     def __init__(self):
         super(ScalingLayer, self).__init__()
-        self.register_buffer('shift', torch.Tensor([-.030,-.088,-.188])[None,:,None,None])
-        self.register_buffer('scale', torch.Tensor([.458,.448,.450])[None,:,None,None])
+        self.register_buffer(
+            "shift", torch.Tensor([-0.030, -0.088, -0.188])[None, :, None, None]
+        )
+        self.register_buffer(
+            "scale", torch.Tensor([0.458, 0.448, 0.450])[None, :, None, None]
+        )
 
     def forward(self, inp):
         return (inp - self.shift) / self.scale
 
 
 class NetLinLayer(nn.Module):
-    ''' A single linear layer which does a 1x1 conv '''
-    def __init__(self, chn_in, chn_out=1, use_dropout=False):
+    """A single linear layer which does a 1x1 conv"""
+
+    def __init__(self, chn_in, chn_out=1):
         super(NetLinLayer, self).__init__()
 
-        layers = [nn.Dropout(),] if(use_dropout) else []
-        layers += [nn.Conv2d(chn_in, chn_out, 1, stride=1, padding=0, bias=False),]
+        layers = [
+            nn.Dropout(),
+        ]
+        layers += [
+            nn.Conv2d(chn_in, chn_out, 1, stride=1, padding=0, bias=False),
+        ]
         self.model = nn.Sequential(*layers)
 
     def forward(self, x):
         return self.model(x)
-
-class Dist2LogitLayer(nn.Module):
-    ''' takes 2 distances, puts through fc layers, spits out value between [0,1] (if use_sigmoid is True) '''
-    def __init__(self, chn_mid=32, use_sigmoid=True):
-        super(Dist2LogitLayer, self).__init__()
-
-        layers = [nn.Conv2d(5, chn_mid, 1, stride=1, padding=0, bias=True),]
-        layers += [nn.LeakyReLU(0.2,True),]
-        layers += [nn.Conv2d(chn_mid, chn_mid, 1, stride=1, padding=0, bias=True),]
-        layers += [nn.LeakyReLU(0.2,True),]
-        layers += [nn.Conv2d(chn_mid, 1, 1, stride=1, padding=0, bias=True),]
-        if(use_sigmoid):
-            layers += [nn.Sigmoid(),]
-        self.model = nn.Sequential(*layers)
-
-    def forward(self,d0,d1,eps=0.1):
-        return self.model.forward(torch.cat((d0,d1,d0-d1,d0/(d1+eps),d1/(d0+eps)),dim=1))
-
-class BCERankingLoss(nn.Module):
-    def __init__(self, chn_mid=32):
-        super(BCERankingLoss, self).__init__()
-        self.net = Dist2LogitLayer(chn_mid=chn_mid)
-        # self.parameters = list(self.net.parameters())
-        self.loss = torch.nn.BCELoss()
-
-    def forward(self, d0, d1, judge):
-        per = (judge+1.)/2.
-        self.logit = self.net.forward(d0,d1)
-        return self.loss(self.logit, per)
-
-# L2, DSSIM metrics
-class FakeNet(nn.Module):
-    def __init__(self, use_gpu=True, colorspace='Lab'):
-        super(FakeNet, self).__init__()
-        self.use_gpu = use_gpu
-        self.colorspace = colorspace
-
-class L2(FakeNet):
-    def forward(self, in0, in1, retPerLayer=None):
-        assert(in0.size()[0]==1) # currently only supports batchSize 1
-
-        if(self.colorspace=='RGB'):
-            (N,C,X,Y) = in0.size()
-            value = torch.mean(torch.mean(torch.mean((in0-in1)**2,dim=1).view(N,1,X,Y),dim=2).view(N,1,1,Y),dim=3).view(N)
-            return value
-        elif(self.colorspace=='Lab'):
-            value = lpips.l2(lpips.tensor2np(lpips.tensor2tensorlab(in0.data,to_norm=False)), 
-                lpips.tensor2np(lpips.tensor2tensorlab(in1.data,to_norm=False)), range=100.).astype('float')
-            ret_var = Variable( torch.Tensor((value,) ) )
-            if(self.use_gpu):
-                ret_var = ret_var.cuda()
-            return ret_var
-
-class DSSIM(FakeNet):
-
-    def forward(self, in0, in1, retPerLayer=None):
-        assert(in0.size()[0]==1) # currently only supports batchSize 1
-
-        if(self.colorspace=='RGB'):
-            value = lpips.dssim(1.*lpips.tensor2im(in0.data), 1.*lpips.tensor2im(in1.data), range=255.).astype('float')
-        elif(self.colorspace=='Lab'):
-            value = lpips.dssim(lpips.tensor2np(lpips.tensor2tensorlab(in0.data,to_norm=False)), 
-                lpips.tensor2np(lpips.tensor2tensorlab(in1.data,to_norm=False)), range=100.).astype('float')
-        ret_var = Variable( torch.Tensor((value,) ) )
-        if(self.use_gpu):
-            ret_var = ret_var.cuda()
-        return ret_var
-
-def print_network(net):
-    num_params = 0
-    for param in net.parameters():
-        num_params += param.numel()
-    print('Network',net)
-    print('Total number of parameters: %d' % num_params)
 
 
 def model_setenv():
@@ -239,9 +150,9 @@ def model_setenv():
 
     # Set default device to avoid exceptions
     if os.environ.get("DEVICE") != "cuda" and os.environ.get("DEVICE") != "cpu":
-        os.environ["DEVICE"] = 'cuda' if torch.cuda.is_available() else 'cpu'
+        os.environ["DEVICE"] = "cuda" if torch.cuda.is_available() else "cpu"
 
-    if os.environ["DEVICE"] == 'cuda':
+    if os.environ["DEVICE"] == "cuda":
         torch.backends.cudnn.enabled = True
         torch.backends.cudnn.benchmark = True
 
@@ -250,15 +161,23 @@ def model_setenv():
     print("  PWD: ", os.environ["PWD"])
     print("  DEVICE: ", os.environ["DEVICE"])
 
+
 def model_device():
-    """Please call after model_setenv. """
+    """Please call after model_setenv."""
     return torch.device(os.environ["DEVICE"])
+
 
 def get_model():
     """Create model."""
     model_setenv()
-    model = LPIPS(net='vgg', spatial=True)
+    model = LPIPS(net="vgg")
     return model
+
+
+def to_numpy(tensor):
+    return (
+        tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
+    )
 
 
 def onnx_model_load(onnx_file):
@@ -266,31 +185,32 @@ def onnx_model_load(onnx_file):
     # sess_options.log_severity_level = 0
 
     # Set graph optimization level
-    sess_options.graph_optimization_level = onnxruntime.GraphOptimizationLevel.ORT_ENABLE_EXTENDED
+    sess_options.graph_optimization_level = (
+        onnxruntime.GraphOptimizationLevel.ORT_ENABLE_EXTENDED
+    )
 
     onnx_model = onnxruntime.InferenceSession(onnx_file, sess_options)
     # onnx_model.set_providers(['CUDAExecutionProvider'])
-    print("Onnx model engine: ", onnx_model.get_providers(), "Device: ", onnxruntime.get_device())
+    print(
+        "Onnx model engine: ",
+        onnx_model.get_providers(),
+        "Device: ",
+        onnxruntime.get_device(),
+    )
 
     return onnx_model
 
-def onnx_model_forward(onnx_model, input):
-    def to_numpy(tensor):
-        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
 
+def onnx_model_forward(onnx_model, input):
     onnxruntime_inputs = {onnx_model.get_inputs()[0].name: to_numpy(input)}
     onnxruntime_outputs = onnx_model.run(None, onnxruntime_inputs)
     return torch.from_numpy(onnxruntime_outputs[0])
 
+
 def export_onnx():
     """Export onnx model."""
 
-    import numpy as np
-    import onnx
-    import onnxruntime
-    from onnx import optimizer
-
-    onnx_file_name = "output/image_ganloss.onnx"
+    onnx_file_name = "output/image_ploss.onnx"
     dummy_input = torch.randn(2, 3, 256, 256).cuda()
 
     # 1. Create and load model.
@@ -303,41 +223,39 @@ def export_onnx():
 
     input_names = ["input"]
     output_names = ["output"]
-    torch.onnx.export(torch_model, dummy_input, onnx_file_name,
-                      input_names=input_names,
-                      output_names=output_names,
-                      verbose=True,
-                      opset_version=11,
-                      keep_initializers_as_inputs=False,
-                      export_params=True)
+    torch.onnx.export(
+        torch_model,
+        dummy_input,
+        onnx_file_name,
+        input_names=input_names,
+        output_names=output_names,
+        verbose=True,
+        opset_version=11,
+        keep_initializers_as_inputs=False,
+        export_params=True,
+    )
 
     # 3. Optimize model
-    print('Checking model ...')
+    print("Checking model ...")
     onnx_model = onnx.load(onnx_file_name)
     onnx.checker.check_model(onnx_model)
     onnx.helper.printable_graph(onnx_model.graph)
     # https://github.com/onnx/optimizer
 
     # 4. Visual model
-    # python -c "import netron; netron.start('output/image_ganloss.onnx')"
+    # python -c "import netron; netron.start('output/image_ploss.onnx')"
+
 
 def verify_onnx():
     """Verify onnx model."""
 
-    import numpy as np
-    import onnxruntime
-
     # ------- For Transformer -----------------------
-    onnx_file_name = "output/image_ganloss.onnx"
+    onnx_file_name = "output/image_ploss.onnx"
     torch_model = get_model()
     torch_model.eval()
     onnxruntime_engine = onnx_model_load(onnx_file_name)
 
-    def to_numpy(tensor):
-        return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
-
     dummy_input = torch.randn(2, 3, 256, 256)
-
     with torch.no_grad():
         torch_output = torch_model(dummy_input)
 
@@ -347,8 +265,9 @@ def verify_onnx():
 
     onnxruntime_outputs = onnxruntime_engine.run(None, onnxruntime_inputs)
     np.testing.assert_allclose(
-        to_numpy(torch_output), onnxruntime_outputs[0], rtol=1e-02, atol=1e-02)
-    print("Loss onnx model has been tested with ONNXRuntime, the result sounds good !")
+        to_numpy(torch_output), onnxruntime_outputs[0], rtol=1e-02, atol=1e-02
+    )
+    print("Loss onnx model tested with ONNXRuntime, result sounds good !")
 
 
 def test_sample():
@@ -361,9 +280,9 @@ def test_sample():
 
     totensor = transforms.ToTensor()
 
-    ex_ref = totensor(Image.open('output/ex_ref.png'));
-    ex_p0 = totensor(Image.open('output/ex_p0.png'));
-    ex_p1 = totensor(Image.open('output/ex_p1.png'));
+    ex_ref = totensor(Image.open("output/ex_ref.png"))
+    ex_p0 = totensor(Image.open("output/ex_p0.png"))
+    ex_p1 = totensor(Image.open("output/ex_p1.png"))
     ex_ref = ex_ref.unsqueeze(0).to(device)
     ex_p0 = ex_p0.unsqueeze(0).to(device)
     ex_p1 = ex_p1.unsqueeze(0).to(device)
@@ -372,29 +291,27 @@ def test_sample():
     with torch.no_grad():
         ex_d0 = model(torch.cat([ex_ref, ex_p0], dim=0))
         ex_d1 = model(torch.cat([ex_ref, ex_p1], dim=0))
-        ex_d2 = model(torch.cat([ex_ref, ex_ref], dim = 0))
-        ex_d3 = model(torch.cat([ex_p0, ex_p1], dim = 0))
+        ex_d2 = model(torch.cat([ex_ref, ex_ref], dim=0))
+        ex_d3 = model(torch.cat([ex_p0, ex_p1], dim=0))
         ex_d4 = model(torch.cat([ex_p1, ex_p0], dim=0))
-    
-    print('Loss: (%.3f, %.3f, %.3f, %.3f, %.3f)'% 
-        (ex_d0.mean(), ex_d1.mean(), ex_d2.mean(), ex_d3.mean(), ex_d4.mean()))
+
+    print(
+        "Loss: (%.3f, %.3f, %.3f, %.3f, %.3f)"
+        % (ex_d0.mean(), ex_d1.mean(), ex_d2.mean(), ex_d3.mean(), ex_d4.mean())
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """Test Tools ..."""
     import argparse
     import os
 
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--export', help="Export onnx model", action='store_true')
-    parser.add_argument(
-        '--verify', help="Verify onnx model", action='store_true')
-    parser.add_argument(
-        '--test', help="Test", action='store_true')
+    parser.add_argument("--export", help="Export onnx model", action="store_true")
+    parser.add_argument("--verify", help="Verify onnx model", action="store_true")
+    parser.add_argument("--test", help="Test", action="store_true")
 
-    parser.add_argument('--output', type=str,
-                        default="output", help="output folder")
+    parser.add_argument("--output", type=str, default="output", help="output folder")
 
     args = parser.parse_args()
 
@@ -409,4 +326,3 @@ if __name__ == '__main__':
 
     if args.test:
         test_sample()
-
